649add48c6420f703eb7098063587c7bf6eef166
wackyjoker <kevin.young8931@gmail.com>
2018-06-28 19:26:29 +0200

    Setup devDependencies 137done

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports=(env)=>{
         mode:isProduction ? 'production' : 'development', // "production" | "development" | "none"
     entry:"./src/app.js",
     output:{
-        path: path.join(__dirname,"public"),
+        path: path.join(__dirname,"public","dist"),
         filename:"bundle.js",
     },
     module:{
@@ -44,7 +44,8 @@ module.exports=(env)=>{
     devServer:{
         contentBase: path.join(__dirname,"public"),
         port: 9000,
-        historyApiFallback:true
+        historyApiFallback:true,
+        publicPath:'/dist/'
         }
     }
 };
